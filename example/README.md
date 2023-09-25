1f4a2e96c17f0abd628042e803b5ca57089d678c
Gregor Kobsik <gregor.kobsik@rwth-aachen.de>
2023-09-26 00:36:58 +0200

    Add a simple example solution and some test data

---
+++ example/README.md
@@ -0,0 +1,24 @@
+# Minimalbeispiel
+
+Hier findet ihr ein kleines Beispiel, wie der Code einer einfachen Beispielabgabe aussehen könnte.
+Natürlich ist es nur eine sehr kurze und rudimäntere Implementierung, aber ihr könnt gerne auf diesem Code aufbauen und ihn verbessern oder auch eine völlig andere Programmiersprache und Codebase verwenden.
+Es ist euer Projekt!
+
+## Benutzung
+
+Das Programm kann mit Python ausgeführt werden. Hierzu benötigt ihr zunächst eine Python-Umgebung mit `skimage`, `pathlib` und `numpy`.
+
+Beispiel für die Augmentierung einer Bilddatei:
+```bash
+python main.py ai_gen_images/clownfish.png output/clownfish_augmented.png
+```
+
+Beispiel für die Augmentierung einer Textdatei:
+```bash
+python main.py ai_gen_text/alpen.txt output/alpen_augmented.txt
+```
+
+## Daten
+
+In den beiden Ordnern `ai_gen_images` und `ai_gen_text` findet ihr Beispieleingaben für Text und Bilder, die von einer KI erstellt worden ist. 
+Wir werden weitere Testdaten im Laufe des Wettbewerbs veröffentlichen, aber ihr könnt auch gerne selbst neue Texte und Bilder für eure Testzwecke generieren.
---
+++ example/main.py
@@ -0,0 +1,45 @@
+import sys
+import skimage
+from pathlib import Path
+import numpy as np
+
+def process_image_file(in_path, out_path):
+    """ Adds some random noise to an image to augment it. """
+    # read image file, 
+    image = skimage.io.imread(in_path)
+    # augment the image
+    augmented_image = skimage.util.random_noise(image, mode='gaussian', clip=True)
+    # convert pixel representation from [0..1] to [0..255]
+    augmented_image = (augmented_image * 255).astype(np.uint8)
+    # save image to given location
+    skimage.io.imsave(out_path, augmented_image, check_contrast=False)
+
+def process_text_file(in_path, out_path):
+    """ Replaces every occurence of a white space with double white space to augment a text. """
+    # open and read a text file
+    text = in_path.read_text()
+    # augment the text file
+    augmented_text = text.replace(" ", "  ")
+    # save text as file to given location
+    out_path.write_text(augmented_text)
+
+if __name__ == '__main__':
+    # check if an input and an output file path was given
+    assert len(sys.argv) == 3 
+
+    # get the given input and output file paths and save them in a variable
+    input_filepath = Path(sys.argv[1])
+    output_filepath = Path(sys.argv[2])
+
+    # check if the input and output filetypes match, otherwise we might need to process the data differently
+    assert input_filepath.suffix == output_filepath.suffix
+
+    # create needed directiories first, if not already created
+    output_filepath.parent.mkdir(parents=True, exist_ok=True)
+
+    # Process files accordingly to suffix
+    if input_filepath.suffix in ('.txt'):
+        process_text_file(input_filepath, output_filepath)
+    elif input_filepath.suffix in ('.png', '.jpg'):
+        process_image_file(input_filepath, output_filepath)
+    
